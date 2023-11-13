26fabb3886c82b561c8f9cfd2c13c9ef3c175b21
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-11-14 04:50:49 +0700

    Created a column for the form

--- app/src/main/java/com/example/navigation/HalamanForm.kt
+++ app/src/main/java/com/example/navigation/HalamanForm.kt
@@ -1,11 +1,18 @@
 package com.example.navigation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,4 +32,12 @@ fun HalamanForm(
     }
 
     var listDataTxt : MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ){}
 }
