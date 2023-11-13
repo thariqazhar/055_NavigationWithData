171d2c573d8c0baf6a75c567d1606a7bfc403902
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-11-14 04:37:48 +0700

    Add new val in OrderUIState data class

--- app/src/main/java/com/example/navigation/data/OrderUIState.kt
+++ app/src/main/java/com/example/navigation/data/OrderUIState.kt
@@ -3,5 +3,8 @@ package com.example.navigation.data
 data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
-    val harga: String = ""
+    val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
 )
