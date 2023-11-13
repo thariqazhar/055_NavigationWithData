9355da70492109ae3cd90708cd79ac2db8ceb558
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-11-14 04:57:05 +0700

    Created a button for submit and added a spacer between the outlined text fields and the button

--- app/src/main/java/com/example/navigation/HalamanForm.kt
+++ app/src/main/java/com/example/navigation/HalamanForm.kt
@@ -2,8 +2,10 @@ package com.example.navigation
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -14,6 +16,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,5 +60,10 @@ fun HalamanForm(
         }, label = {
             Text(text = "No. Telepon")
         })
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Button(onClick =  {onSubmitButtonClick(listDataTxt)} ) {
+            Text(text = stringResource(R.string.btn_submit))
+        }
     }
 }
